7f678fe459cbd06ae84c8f3131843bb8f6680f49
ghostplant <12099308+ghostplant@users.noreply.github.com>
2020-09-09 12:37:15 +0800

    output detailed CUDA intermediate data (#3)

--- platforms/c-cuda/config.py
+++ platforms/c-cuda/config.py
@@ -8,8 +8,9 @@ def get_execution_parallism():
 
 def get_compile_kernel_args(kernel_src, kernel_out, device_props):
   code_arch = device_props.compute_version.replace('.', '')
-  os.system('ln -sf %s %s.cu' % (kernel_src, kernel_src))
-  return ['/usr/local/cuda/bin/nvcc', kernel_src + '.cu', '--fatbin', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', kernel_out]
+  assert 0 == os.system('ln -sf %s %s.cu' % (kernel_src, kernel_src))
+  assert 0 == os.system(' '.join(['/usr/local/cuda/bin/nvcc', kernel_src + '.cu', '--ptx', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', '%s.ptx' % kernel_src]))
+  return ['/usr/local/cuda/bin/nvcc', kernel_src + '.ptx', '--fatbin', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', kernel_out]
 
 def do_native_translation(code, **kwargs):
   code = '#include <cuda_runtime.h>\n#include <cuda_fp16.h>\n\n' + kwargs['attrs'].blend + '\n' + code
