07eddf3c51cd9f91c79a21fa8b7a05f32f8d00d6
Wei CUI <ghostplant@qq.com>
2020-09-10 13:38:53 +0800

    fix cast translation

--- platforms/c-cuda/config.py
+++ platforms/c-cuda/config.py
@@ -9,7 +9,7 @@ def get_execution_parallism():
 def get_compile_kernel_args(kernel_src, kernel_out, device_props):
   code_arch = device_props.compute_version.replace('.', '')
   assert 0 == os.system('ln -sf %s %s.cu' % (kernel_src, kernel_src))
-  assert 0 == os.system(' '.join(['/usr/local/cuda/bin/nvcc', kernel_src + '.cu', '--ptx', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', '%s.ptx' % kernel_src]))
+  os.system(' '.join(['/usr/local/cuda/bin/nvcc', kernel_src + '.cu', '--ptx', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', '%s.ptx' % kernel_src]))
   return ['/usr/local/cuda/bin/nvcc', kernel_src + '.ptx', '--fatbin', '-O2', '-gencode', 'arch=compute_%s,code=sm_%s' % (code_arch, code_arch), '-o', kernel_out]
 
 def do_native_translation(code, **kwargs):
