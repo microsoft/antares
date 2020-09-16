32985bc17ccac15c7c77aa113d9331f0345c896f
ghostplant <12099308+ghostplant@users.noreply.github.com>
2020-09-16 13:35:32 +0800

    add local cache elimination for c-mcpu (#15)

--- platforms/c-mcpu/config.py
+++ platforms/c-mcpu/config.py
@@ -14,6 +14,18 @@ def get_compile_kernel_args(kernel_src, kernel_out, device_props):
 def allow_concurrent_compile_execution():
     return False
 
+def remove_local_cache(code, arg_bufs):
+    result = []
+    for line in code.split('\n'):
+      if line.endswith('];') and line.find('=') < 0:
+        output_buf = arg_bufs['_out'][0]
+        print(line.split()[0], output_buf['dtype'])
+        if line.split()[0] != _native_dtype(output_buf['dtype']):
+          raise Exception("This backend doesn't support injective computation modifying the output type")
+        line = '  ' + line.split('[')[0].strip().replace(' ', ' *') + ' = &' + output_buf['name'] + '[0];'
+      result.append(line)
+    return '\n'.join(result)
+
 def do_native_translation(code, **kwargs):
     arg_bufs = AntaresGlobal.current_arg_bufs
 
@@ -29,5 +41,5 @@ def do_native_translation(code, **kwargs):
     code = 'extern "C" void kernel_main(%s) {\n  // [thread_compute]\n' % ', '.join([t + '* ' + v for t, v in args]) + code[tail + len(") {\n"):]
     code = code.replace('threadIdx.x', '__rank__').replace(' __global__ ', ' ').replace(' __restrict__ ', ' ')
     code = '#include <math.h>\n#include <algorithm>\nusing namespace std;\n\n' + kwargs['attrs'].blend + '\n' + code
-
+    code = remove_local_cache(code, arg_bufs)
     return code
