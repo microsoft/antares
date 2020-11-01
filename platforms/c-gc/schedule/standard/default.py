43e14b306be9a94438abc00b5faded45afadead5
ghostplant <12099308+ghostplant@users.noreply.github.com>
2020-11-01 21:00:31 +0800

    partial upgrade (#36)
    Co-authored-by: Wei CUI <weicu@microsoft.com>

--- platforms/c-gc/schedule/standard/default.py
+++ platforms/c-gc/schedule/standard/default.py
@@ -17,29 +17,18 @@ def schedule(antares):
     for i in range(len(output.op.axis)):
       ax_name = 'axis_%d' % i
       cfg.define_split(ax_name, cfg.axis(output.op.axis[i]), num_outputs=2)
-    num_cores, align_width = 1216, 64
-    cfg.define_knob('start_core', [x * align_width for x in range(num_cores // align_width)])
+    # num_cores, align_width = 1216, 64
+    # cfg.define_knob('start_core', [x * align_width for x in range(num_cores // align_width)])
     return
-
-  # Local scheduling plan
-  # if rd_vals:
-  #   if output.op in s.outputs:
-  #     output_local = s.cache_write(output, "local")
-  #   else:
-  #     s[output].set_scope('local')
-  #     output_local, output = output, s.outputs[0].output(0)
 
   loop_axes = []
   for i in range(len(th_vals)):
     lo, li = s[output].split(output.op.axis[i], nparts=1)
     if i == 0:
-      s[output].bind(lo, te.thread_axis('threadIdx.x'))
+      s[output].bind(lo, te.thread_axis('blockIdx.x'))
     else:
       li = s[output].fuse(lo, li)
     s[output].bind(li, te.thread_axis('vthread'))
     loop_axes.append(li)
 
-  s[output].reorder(*(loop_axes))
-
-  # if rd_vals:
-  #   s[output_local].compute_at(s[output], loop_axes[-1])
+  s[output].reorder(*reversed(loop_axes))
