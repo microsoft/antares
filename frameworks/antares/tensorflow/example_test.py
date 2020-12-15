6f8be85b3c6ae8cbc8bb869d9be8bb66cffe00c5
ghostplant <12099308+ghostplant@users.noreply.github.com>
2020-12-15 09:43:34 +0000

    allow tf plugin to use map-type inputs (#110)

--- frameworks/antares/tensorflow/example_test.py
+++ frameworks/antares/tensorflow/example_test.py
@@ -8,11 +8,10 @@ from tensorflow.contrib import antares
 import os
 
 x = tf.random.uniform([1024, 512])
-y = tf.random.uniform([1024, 512])
 
-op = antares.make_op('output0[N, M] = input0[N, M] * input1[N, M] + 1234', [x, y], server_addr=os.environ.get('ANTARES_ADDR', 'localhost:8880'))
+op = antares.make_op('reduce_sum_0[N] +=! data[N, M]', {'data': x}, server_addr=os.environ.get('ANTARES_ADDR', 'localhost:8880'))
 
 with tf.Session() as sess:
-  print(sess.run(op))
+  print('The result of tensor `%s` is:\n%s' % (op._output_names[0], sess.run(op)))
   sess.run([op] * 100)
 
