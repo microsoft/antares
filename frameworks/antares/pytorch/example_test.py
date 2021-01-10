321eef7171ae794573a1b5915583a0b3dcdf4ecc
Wei CUI <ghostplant@qq.com>
2021-01-10 21:34:43 +0800

    complete pytorch tuning interface

--- frameworks/antares/pytorch/example_test.py
+++ frameworks/antares/pytorch/example_test.py
@@ -9,7 +9,6 @@ from torch.contrib.antares.custom_op import CustomOp
 
 device = torch.device("cuda")
 dtype = torch.float32
-custom_op = CustomOp().to(device, dtype)
 
 kwargs = {'dtype': dtype,
           'device': device,
@@ -18,5 +17,7 @@ kwargs = {'dtype': dtype,
 x = torch.ones(128, 1024, **kwargs)
 y = torch.ones(1024, 1024, **kwargs)
 
-result = custom_op(ir='dot_0[N, M] +=! data[N, K] * weight[K, M]', mapped_keys=['data', 'weight'], mapped_values=[x, y])
+custom_op = CustomOp(ir='dot_0[N, M] +=! data[N, K] * weight[K, M]', feed_dict={'data': x, 'weight': y}).to(device, dtype).tune(step=100, use_cache=True, timeout=600).emit()
+
+result = custom_op()
 print('The result of tensor `%s` is:\n%s' % (result.id, result))
