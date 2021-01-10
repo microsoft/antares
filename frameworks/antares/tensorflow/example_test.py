eeb0d3664efb02a225abf40ab19361e44f1d56ae
Wei CUI <ghostplant@qq.com>
2021-01-10 15:55:02 +0800

    add tune & emit in TF1 JIT-plugin

--- frameworks/antares/tensorflow/example_test.py
+++ frameworks/antares/tensorflow/example_test.py
@@ -5,13 +5,13 @@
 
 import tensorflow as tf
 from tensorflow.contrib import antares
-import os
 
-x = tf.random.uniform([1024, 512])
+x = tf.get_variable('x', [128, 1024], tf.float32, initializer=tf.initializers.ones(tf.float32), trainable=False)
+y = tf.get_variable('y', [1024, 1024], tf.float32, initializer=tf.initializers.ones(tf.float32), trainable=False)
 
-op = antares.make_op('reduce_sum_0[N] +=! data[N, M]', {'data': x}, server_addr=os.environ.get('ANTARES_ADDR', 'localhost:8880'))
+op = antares.make_op(ir='dot_0[N, M] +=! data[N, K] * weight[K, M]', feed_dict={'data': x, 'weight': y}).tune(step=100, use_cache=True).emit()
 
 with tf.Session() as sess:
-  print('The result of tensor `%s` is:\n%s' % (op._output_names[0], sess.run(op)))
-  sess.run([op] * 100)
+  sess.run(tf.global_variables_initializer())
+  print('The result of tensor `%s` is:\n%s' % (op, sess.run(op)))
 
