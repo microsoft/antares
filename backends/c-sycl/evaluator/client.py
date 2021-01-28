cca2c441ac496f2b8caf4353f20492bddfe4786b
ghostplant <12099308+ghostplant@users.noreply.github.com>
2021-01-28 08:00:02 +0000

    minor fixes (#150)

--- backends/c-sycl/evaluator/client.py
+++ backends/c-sycl/evaluator/client.py
@@ -10,15 +10,13 @@ from antares.common import backend
 def eval(kernel_path, **kwargs):
     source_file = '%s/run_graph.cpp' % os.path.dirname(__file__)
     dest_dir = os.path.dirname(kernel_path)
-    assert 0 == os.system(f'timeout 10s dpcpp {source_file} -I{dest_dir} -std=c++17 -lpthread -o {dest_dir}/run_graph'), "Compiling error or DPC++ SDK not found, please check the oneAPI environment."
+    assert 0 == os.system(f'dpcpp {source_file} -I{dest_dir} -std=c++17 -lpthread -o {dest_dir}/run_graph'), "Compiling error or DPC++ SDK not found, please check the oneAPI environment."
     
     exec_cmd = 'sh -c "cd %s && EXPECTED_TIMEOUT=%s %s"' % (dest_dir, kwargs['expected_timeout'], './run_graph')
     try:
       output = subprocess.check_output(exec_cmd, shell=True).decode()
     except:
       raise Exception("Invalid runtime kernel execution: %s\n" % (exec_cmd))
-
-    print(output)
 
     results = {}
     for line in output.split('\n'):
