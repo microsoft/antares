9a6d984c4fa9863c72ff5da4e30a6d25b082ef39
Wei CUI <ghostplant@qq.com>
2021-02-23 10:22:11 +0800

    refine partial codes for mingw64

--- graph_evaluator/client.py
+++ graph_evaluator/client.py
@@ -4,6 +4,7 @@
 import os, sys, time, math
 import numpy as np
 import subprocess
+import hashlib
 
 def init(**kwargs):
     backend_root = kwargs['backend_root']
@@ -23,23 +24,24 @@ def init(**kwargs):
         exit(1)
       return eval_client.init(**kwargs)
 
-    with open(f'{backend_root}/include/backend.hpp', 'r') as fp:
-      eval_flags_pref = f'//; eval_flags({backend}):'
-      eval_flags, compiler = '', 'g++'
-      while True:
-        line = fp.readline()
-        if not line:
-          break
-        line = line.strip()
-        if line.startswith(eval_flags_pref):
-          eval_flags = line[len(eval_flags_pref):].strip()
-          if eval_flags.startswith('['):
-            idx = eval_flags.index(']')
-            eval_flags, compiler = eval_flags[idx+1:].strip(), eval_flags[1:idx].strip()
-          break
-
     evaluator_path = '%s/evaluator.%s' % (os.environ['ANTARES_DRIVER_PATH'], backend)
+
     if not os.path.exists(evaluator_path):
+      with open(f'{backend_root}/include/backend.hpp', 'r') as fp:
+        eval_flags_pref = f'//; eval_flags({backend}):'
+        eval_flags, compiler = '', 'g++'
+        while True:
+          line = fp.readline()
+          if not line:
+            break
+          line = line.strip()
+          if line.startswith(eval_flags_pref):
+            eval_flags = line[len(eval_flags_pref):].strip()
+            if eval_flags.startswith('['):
+              idx = eval_flags.index(']')
+              eval_flags, compiler = eval_flags[idx+1:].strip(), eval_flags[1:idx].strip()
+            break
+
       error_info = f"SDK for `{backend}` is not configured correctly, please look into the error messages and reconfigure the corresponding environment."
       pre_define_macro = backend.upper().replace('-', '_')
       assert 0 == os.system(f'timeout 10s {compiler} {source_root}/run_graph.cpp -D__BACKEND__={pre_define_macro} -I{backend_root}/include -std=c++17 -Wno-unused-result -Wno-unused-value -lpthread -o {evaluator_path}.tmp {eval_flags}'), error_info
