d2a09a8e0f1bf2f1f4a7d9c318ff0513bc3ae410
ghostplant <12099308+ghostplant@users.noreply.github.com>
2021-03-01 15:42:55 +0800

    add remote tuning source code for c-hlsl_xbox (#186)

---
+++ backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Downloading dependencies ..
+curl -LOs https://github.com/microsoft/antares/releases/download/v0.1.0/antares_hlsl_v0.2dev0_x64.dll
+
+echo Compiling helloworld example ..
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe helloworld.cs
+
+echo Compiling finished!
+pause
+
+echo Executing program ..
+helloworld
+
+echo Program finished!
+pause
