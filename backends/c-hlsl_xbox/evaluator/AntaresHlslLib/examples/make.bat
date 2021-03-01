7b2983daf6e70d71e55e414aeece6e5f8b9dcbba
ghostplant <12099308+ghostplant@users.noreply.github.com>
2021-03-01 16:02:29 +0800

    update script conditions (#187)

--- backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
+++ backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
@@ -1,7 +1,19 @@
 @echo off
 
-echo Downloading dependencies ..
-curl -LOs https://github.com/microsoft/antares/releases/download/v0.1.0/antares_hlsl_v0.2dev0_x64.dll
+cd %~dp0
+
+if exist antares_hlsl_v0.2dev0_x64.dll (
+  echo Using local dependencies ..
+) else (
+  echo Downloading dependencies ..
+  curl.exe -LOs https://github.com/microsoft/antares/releases/download/v0.1.0/antares_hlsl_v0.2dev0_x64.dll
+
+  if errorlevel 1 (
+    echo "`curl.exe` command not found in Windows PATH. Failed to download dependencies."
+    pause
+    exit
+  )
+)
 
 echo Compiling helloworld example ..
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe helloworld.cs
