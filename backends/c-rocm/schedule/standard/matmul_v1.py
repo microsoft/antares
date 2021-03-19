7af7e6f28917bab614654a067fc6f6e5fc53f953
Wei CUI <ghostplant@qq.com>
2021-03-19 17:39:53 +0800

    many fixes

--- backends/c-rocm/schedule/standard/matmul_v1.py
+++ backends/c-rocm/schedule/standard/matmul_v1.py
@@ -9,7 +9,6 @@ import sys, time, subprocess
 
 def schedule(attrs):
     cfg, s, output = attrs.auto_config, attrs.scheduler, attrs.explicit_ops[-1].output(0)
-    th_vals, rd_vals = [attrs.get_extent(x) for x in output.op.axis], [attrs.get_extent(x) for x in output.op.reduce_axis]
 
     y, x = s[output].op.axis
     [rc] = s[output].op.reduce_axis
@@ -17,7 +16,12 @@ def schedule(attrs):
     cfg.define_split("tile_x", x, num_outputs=4)
     cfg.define_split("tile_rc", rc, num_outputs=3)
 
-    data_deform, kernel = s[output].op.input_tensors
+    shared_list = []
+    for I in s[output].op.input_tensors:
+      shared_list.append(I)
+
+    num_threads = cfg["tile_x"].size[2] * cfg["tile_y"].size[2]
+    assert num_threads <= 1024, "Invalid schedule plans: num_threads(%d) > 1024" % num_threads
 
     if output.op in s.outputs:
         output = output
@@ -43,33 +47,34 @@ def schedule(attrs):
 
     s[output].bind(b_fused, te.thread_axis("blockIdx.x"))
     s[output].bind(v_fused, te.thread_axis("vthread"))
-    s[output].bind(t_fused, te.thread_axis("threadIdx.x"))
+    s[output].bind(t_fused, te.thread_axis("threadIdx.y"))
 
-    # create cache stage
-    AA = s.cache_read(data_deform, 'shared', [OL])
-    WW = s.cache_read(kernel, 'shared', [OL])
+    shared_list = []
+    for I in shared_list:
+      IS = s.cache_read(I, 'shared', [OL])
+      shared_list.append(IS)
 
     # tile reduction axes
     y, x = s[OL].op.axis
     [rc] = s[OL].op.reduce_axis
     s[OL].reorder(rco, rcm, rci, y, x)
-
     cache_loc = [rco][-1]
-    s[AA].compute_at(s[OL], cache_loc)
-    s[WW].compute_at(s[OL], cache_loc)
+
+    for IS in shared_list:
+      s[IS].compute_at(s[OL], cache_loc)
 
     # cooperative fetching
-    for i, load in enumerate([AA, WW]):
-        fused_o = s[load].fuse(*s[load].op.axis)
-        cfg.define_knob(f"vectorize_{i}", [0, 2, 4])
-        if cfg[f"vectorize_{i}"].val:
-          fused_o, fused_i = s[load].split(fused, factor=cfg[f"vectorize_{i}"].val)
-          s[load].vectorize(fused_i)
-        fused_o, fused_i = s[load].split(fused_o, factor=cfg["tile_x"].size[2] * cfg["tile_y"].size[2])
-        s[load].bind(fused_i, te.thread_axis("threadIdx.x"))
+    for i, load in enumerate(shared_list):
+      fused_o = s[load].fuse(*s[load].op.axis)
+      cfg.define_knob(f"vectorize_{i}", [0, 2, 4])
+      if cfg[f"vectorize_{i}"].val:
+        fused_o, fused_i = s[load].split(fused_o, factor=cfg[f"vectorize_{i}"].val)
+        s[load].vectorize(fused_i)
+      fused_o, fused_i = s[load].split(fused_o, factor=num_threads)
+      s[load].bind(fused_i, te.thread_axis("threadIdx.y"))
 
     # unroll
-    cfg.define_knob("auto_unroll_max_step", [0, 64, 128, 512])
+    cfg.define_knob("auto_unroll_max_step", [(1 << x) for x in range(10)])
     cfg.define_knob("unroll_explicit", [False, True])
     kernel_scope = rco
     s[OL].pragma(kernel_scope, 'auto_unroll_max_step', cfg['auto_unroll_max_step'].val)
