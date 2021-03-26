3dec579dde2ff1a31a48c5d548395ce3a89219f5
Wei CUI <ghostplant@qq.com>
2021-03-26 18:45:22 +0800

    using dxcompiler by default and enable cs_6_0 shaders

--- backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
+++ backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
@@ -2,11 +2,13 @@
 
 cd %~dp0
 
-if exist antares_hlsl_v0.2_x64.dll (
+if exist dxcompiler.dll (
   echo Using local dependencies ..
 ) else (
   echo Downloading dependencies ..
   curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/antares_hlsl_v0.2dev3_x64.dll -o antares_hlsl_v0.2_x64.dll
+  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/dxcompiler.dll -o dxcompiler.dll
+  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/dxil.dll -o dxil.dll
 
   if errorlevel 1 (
     echo "`curl.exe` command not found in Windows PATH. Failed to download dependencies."
