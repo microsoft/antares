b3920ca760b7f4329416f912cd1a3aa042f9d5b2
Wei CUI <ghostplant@qq.com>
2021-04-03 19:58:51 +0800

    synchronize doc for v0.2.0

--- backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
+++ backends/c-hlsl_xbox/evaluator/AntaresHlslLib/examples/make.bat
@@ -5,10 +5,10 @@ cd %~dp0
 if exist dxcompiler.dll (
   echo Using local dependencies ..
 ) else (
-  echo Downloading dependencies ..
-  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/antares_hlsl_v0.2dev3_x64.dll -o antares_hlsl_v0.2_x64.dll
-  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/dxcompiler.dll -o dxcompiler.dll
-  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.1.0/dxil.dll -o dxil.dll
+  echo Downloading dependencies (Microsoft DirectX Shader Compiler 6) ..
+  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.2.0/antares_hlsl_v0.2_x64.dll -o antares_hlsl_v0.2_x64.dll
+  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.2.0/dxcompiler.dll -o dxcompiler.dll
+  curl.exe -Ls https://github.com/microsoft/antares/releases/download/v0.2.0/dxil.dll -o dxil.dll
 
   if errorlevel 1 (
     echo "`curl.exe` command not found in Windows PATH. Failed to download dependencies."
