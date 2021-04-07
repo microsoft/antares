988f583dabca9ff9b4848f44fed41272627ce893
ghostplant <weicu@microsoft.com>
2021-04-07 06:47:32 +0000

    update android launcher to avoid using /sdcard (#231)

--- backends/c-ocl_android/launcher.sh
+++ backends/c-ocl_android/launcher.sh
@@ -6,6 +6,5 @@ kernel="$2"
 [[ "$executor" == "" ]] && exit 1
 [[ "$kernel" == "" ]] && exit 1
 
-adb push $1 /sdcard/evaluator
-adb push $kernel /sdcard/kernel.cc
-adb shell "su -c 'pkill evaluator -9; mount -o rw,remount /system && cd /system && cp /sdcard/evaluator . && chmod a+x evaluator && ./evaluator /sdcard/kernel.cc'"
+cat "$1" | adb shell "su -c 'pkill evaluator -9; mount -o rw,remount /system; dd of=/system/evaluator && chmod a+x /system/evaluator'"
+cat "$kernel" | adb shell "su -c 'dd of=/system/kernel.cc && /system/evaluator /system/kernel.cc'"
