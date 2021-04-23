5eaab1ba04b8282f22d116a7abbffa2ed18c1d36
ghostplant <weicu@microsoft.com>
2021-04-23 08:41:58 +0000

    upgrade tvm base version (#247)

--- engine/install_antares_host.sh
+++ engine/install_antares_host.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)/..
 ANTARES_ROOT=$(pwd)
 
-VERSION_TAG=v0.2dev5
+VERSION_TAG=v0.2dev6
 
 REQUIRED_PACKAGES="git python3-dev python3-pip g++ llvm-dev make curl libopenmpi-dev openmpi-bin"
 
@@ -20,11 +20,11 @@ dpkg -L ${REQUIRED_PACKAGES} >/dev/null 2>&1 || \
   sh -c "apt-get update && apt-get install -y --no-install-recommends ${REQUIRED_PACKAGES}"
 
 TVM_HOME=/opt/tvm
-GIT_COMMIT=73f425d
+GIT_COMMIT=a33eff5059
 
 if [ ! -e ${TVM_HOME} ] || ! sh -c "cd ${TVM_HOME} && git fetch && git reset --hard && git checkout ${GIT_COMMIT}"; then
   rm -rf ${TVM_HOME}
-  git clone https://github.com/apache/incubator-tvm ${TVM_HOME}
+  git clone https://github.com/apache/tvm ${TVM_HOME}
   sh -c "cd ${TVM_HOME} && git checkout ${GIT_COMMIT}"
 fi
 
@@ -36,12 +36,12 @@ python3 -m pip install --upgrade pip cmake==3.18.0 setuptools && \
   echo '' > /tmp/device-stub.c && gcc /tmp/device-stub.c -shared -o ${TVM_HOME}/device-stub/lib64/libcudart.so && \
   rm -f /tmp/device-stub.c
 
-git checkout 73f425d && git apply device-stub/tvm_v0.7.patch && \
+git checkout ${GIT_COMMIT} && git apply device-stub/tvm_v0.7.patch && \
   git submodule init && git submodule update && \
   mkdir -p build && cd build && cp ../cmake/config.cmake . && \
   sed -i 's/LLVM OFF/LLVM ON/g' config.cmake && sed -i 's~CUDA OFF~CUDA '"${TVM_HOME}/device-stub"'~g' config.cmake && \
   cmake .. && make -j8
 
-python3 -m pip install --upgrade tornado psutil xgboost==1.2.1 numpy decorator attrs pytest typed_ast mpi4py
+python3 -m pip install --upgrade tornado psutil xgboost==1.2.1 numpy decorator attrs pytest typed_ast cloudpickle
 
 echo "$VERSION_TAG" > $TVM_HOME/VERSION_TAG
