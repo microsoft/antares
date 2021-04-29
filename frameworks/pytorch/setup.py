612a7663c691a9f97b46ba601cd4b5b944c55aaf
Wei CUI <ghostplant@qq.com>
2021-04-29 14:10:31 +0000

    support mcpu tuning in pytorch JIT plugin

--- frameworks/pytorch/setup.py
+++ frameworks/pytorch/setup.py
@@ -10,7 +10,7 @@ if len(sys.argv) <= 1:
   sys.argv += ['install']
 
 import torch
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension, IS_HIP_EXTENSION
+from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension, IS_HIP_EXTENSION
 
 dist_path = os.path.join(torch.__path__[0], 'contrib/antares')
 root_path = os.path.dirname(sys.argv[0])
@@ -36,23 +36,30 @@ except FileExistsError:
 shutil.copyfile(root_path + '/custom_op.py', dist_path + '/custom_op.py')
 shutil.copyfile(root_path + '/../../graph_evaluator/execute_module.hpp', dist_path + '/execute_module.hpp')
 
-if torch.cuda.is_available():
-  shutil.copyfile(root_path + '/../../backends/c-rocm/include/backend.hpp', dist_path + '/backend.hpp')
-  is_cuda = not IS_HIP_EXTENSION
-else:
+cpp_flags = [f'-I{dist_path}', '-Wno-sign-compare', '-Wno-address', '-Wno-unused-value']
+
+try:
+  if torch.cuda.is_available():
+    shutil.copyfile(root_path + '/../../backends/c-rocm/include/backend.hpp', dist_path + '/backend.hpp')
+    backend = 'c-cuda' if not IS_HIP_EXTENSION else 'c-rocm'
+    cpp_flags += [f'-D__BACKEND__=\"{backend}\"', '-DANTARES_CUDA' if not IS_HIP_EXTENSION else '-DANTARES_ROCM']
+    ext = CUDAExtension(
+            'antares_custom_op',
+            ['main_ops.cc'],
+            libraries=['cuda'] if not IS_HIP_EXTENSION else [],
+            extra_compile_args={'cxx': cpp_flags, 'nvcc': cpp_flags},
+        )
+  else:
+    raise
+except:
   shutil.copyfile(root_path + '/../../backends/c-mcpu/include/backend.hpp', dist_path + '/backend.hpp')
-  is_cuda = False
+  backend = 'c-mcpu_avx512' if os.system("grep -r '\\bavx512' /proc/cpuinfo >/dev/null") == 0 else 'c-mcpu'
+  cpp_flags += [f'-D__BACKEND__=\"{backend}\"', '-DANTARES_MCPU']
+  ext = CppExtension('antares_custom_op', ['main_ops.cc'], extra_compile_args={'cxx': cpp_flags}, language='c++')
 
 setup(
     name='antares_custom_op',
-    ext_modules=[
-        CUDAExtension(
-            'antares_custom_op',
-            ['main_ops.cc.cu'],
-            libraries=['cuda'] if is_cuda else [],
-            extra_compile_args={'cxx': [f'-I{dist_path}'], 'nvcc': [f'-I{dist_path}']},
-        ),
-    ],
+    ext_modules=[ext,],
     cmdclass={
         'build_ext': BuildExtension
     })
