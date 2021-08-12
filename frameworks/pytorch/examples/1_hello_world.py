8f6604300d9ee0b05791f92a1ee9b0f307d1c278
Wei CUI <ghostplant@qq.com>
2021-08-12 06:27:45 +0000

    update examples of reusing custom_op

--- frameworks/pytorch/examples/1_hello_world.py
+++ frameworks/pytorch/examples/1_hello_world.py
@@ -13,10 +13,13 @@ kwargs = {'dtype': dtype,
           'device': device,
           'requires_grad': False}
 
-x = torch.ones(128, 1024, **kwargs)
-y = torch.ones(1024, 1024, **kwargs)
+x = torch.ones(2, 32, **kwargs)
+y = torch.ones(32, 32, **kwargs)
 
 custom_op = CustomOp(ir='dot_0[N, M] +=! data[N, K] * channels[K, M]', input_orders={'data': x, 'channels': y}).to(device, dtype).tune(step=100, use_cache=True, timeout=600).emit()
 
-result = custom_op(x, y)
+for i in range(4):
+    x = custom_op(x, y)
+    print(f'STEP-{i}: {x.view(-1)}')
+
 print('The result of tensor `%s` is:\n%s' % (custom_op.output_names[0], result))
