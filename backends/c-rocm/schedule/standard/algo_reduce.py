40b7347de56922ff848ff9ca695a62ed9ff5d35a
ghostplant <weicu@microsoft.com>
2021-09-11 08:56:17 +0000

    update algo_reduce policy (#301)

--- backends/c-rocm/schedule/standard/algo_reduce.py
+++ backends/c-rocm/schedule/standard/algo_reduce.py
@@ -6,11 +6,13 @@ from tvm import te
 def schedule_branch(attrs, output, prefix):
   cfg, s = attrs.auto_config, attrs.scheduler
 
+  use_wrap_reduce = cfg.define_knob(f"{prefix}W", [False, True])
   rax = cfg.define_knob(f"{prefix}S", [x for x in range(len(s[output].op.reduce_axis))])
   for i, ax in enumerate(s[output].op.reduce_axis):
     sizes = cfg.define_split(f"{prefix}R{i}", attrs.get_extent(ax), num_outputs=2)
     if rax == i:
-      r_range = max(2, sizes[1])
+      r_range = attrs.device_props.warp_size if use_wrap_reduce else sizes[1]
+      r_range = max(r_range, 2)
       ko, ki = s[output].split(ax, factor=r_range)
       BF = s.rfactor(output, ki)
 
@@ -22,11 +24,11 @@ def schedule_branch(attrs, output, prefix):
   first, second = [x[0] for x in data_slices], [x[1] for x in data_slices]
   s[output].reorder(*(first + second))
 
-  fused_b = s[output].fuse(*first)
-  fused_t = s[output].fuse(*second)
+  fused_bx = s[output].fuse(*first)
+  fused_by = s[output].fuse(*second)
 
-  s[output].bind(fused_b, te.thread_axis("blockIdx.x"))
-  s[output].bind(fused_t, te.thread_axis("threadIdx.y"))
+  s[output].bind(fused_bx, te.thread_axis("blockIdx.x"))
+  s[output].bind(fused_by, te.thread_axis("blockIdx.y"))
 
   rax = min(rax, len(s[output].op.reduce_axis) - 1)
   reduce_ax = s[output].op.reduce_axis[rax]
