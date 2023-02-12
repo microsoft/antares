1a102320bf2efdffee235a5325f9159c28647734
ghostplant <ghostplant@qq.com>
2023-02-12 22:47:20 +0800

    fix template typename sycl atomics

--- backends/c-hlsl_win64/config.py
+++ backends/c-hlsl_win64/config.py
@@ -171,4 +171,5 @@ void CSMain(uint3 threadIdx: SV_GroupThreadID, uint3 blockIdx: SV_GroupID) {{
     full_body = re.sub(r'\(char\)', '', full_body)
     full_body = re.sub(r'\bchar\b', 'bool', full_body)
     full_body = re.sub(r'\bfp16_', '', full_body)
+    full_body = re.sub(fr'\b__device__\b', '', full_body)
     return full_body
